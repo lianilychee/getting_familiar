70e09bfa98a5815f792d99c345dea597f5cc4b5f
Ian Hill <ian.hill@students.olin.edu>
2015-09-13 13:45:36 -0400

    Non-blocking key input for teleop.

---
+++ src/teleop.py
@@ -0,0 +1,34 @@
+import sys
+import select
+import tty
+import termios
+import time
+
+class NonBlockingConsole(object):
+
+    def __enter__(self):
+        self.old_settings = termios.tcgetattr(sys.stdin)
+        tty.setcbreak(sys.stdin.fileno())
+        return self
+
+    def __exit__(self, type, value, traceback):
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.old_settings)
+
+
+    def get_data(self):
+        if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
+            return sys.stdin.read(1)
+        return False
+
+
+if __name__ == '__main__':
+    # Use like this
+    with NonBlockingConsole() as nbc:
+        while 1:
+            key = nbc.get_data()
+            if key == '\x03':  # control + C
+                break
+            if key:
+                print key
+            print "Hello World"
+            time.sleep(.1)
