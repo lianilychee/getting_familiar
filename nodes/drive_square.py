530a11682a8980084c4b07be10fa8abeb7311908
Liani Lye <lye.liani@gmail.com>
2015-09-13 18:09:56 -0400

    bot drives in a 1x1 sq

--- nodes/drive_square.py
+++ nodes/drive_square.py
@@ -1,1 +1,27 @@
-""" Write a ROS node to move the bot in a 1m x 1m square. """
+#!/usr/bin/env python
+
+""" Write a ROS node to move the bot in a (roughly) 1m x 1m square using odometry. """
+
+import rospy
+from geometry_msgs.msg import Twist, Vector3, Quaternion, Point
+from nav_msgs.msg import Odometry
+
+rospy.init_node('square')
+
+def charge():
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+
+    twist = Twist()
+    twist.linear.x = .1
+    pub.publish(twist)
+    rospy.sleep(10)
+
+    twist.angular.z = 1
+    pub.publish(twist)
+    rospy.sleep(1.5);
+
+
+r=rospy.Rate(10)
+
+while not rospy.is_shutdown(): 
+    charge()
