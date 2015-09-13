c789d20cd6d2c23d8e195dcfb74a5acddaca1ddf
Ian Hill <ian.hill@students.olin.edu>
2015-09-13 10:49:20 -0400

    Added forward marker and 2 screenshots.

---
+++ src/scripts/forward_viz_marker.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import roslib
+from visualization_msgs.msg import Marker
+import rospy
+import math
+
+topic = "visualization_marker"
+publisher = rospy.Publisher(topic, Marker, queue_size=10)
+rospy.init_node('register')
+
+marker = Marker()
+marker.header.frame_id = "base_link";
+marker.ns = "my_namespace";
+marker.type = marker.SPHERE
+marker.action = marker.ADD
+marker.scale.x = 0.2
+marker.scale.y = 0.2
+marker.scale.z = 0.2
+marker.color.a = 1.0
+marker.color.r = 1.0
+marker.color.g = 1.0
+marker.color.b = 0.0
+marker.pose.orientation.w = 1.0
+marker.pose.position.x = 1
+marker.pose.position.y = 0
+marker.pose.position.z = 0
+
+r = rospy.Rate(10)
+while not rospy.is_shutdown():
+  publisher.publish(marker)
+  r.sleep()
